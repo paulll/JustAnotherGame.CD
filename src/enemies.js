f1c6a474eb7302c0b786ce7f78463a31df6cfb53
paulll <panovpaul@gmail.com>
2013-12-18 23:48:35 +0400

    enemies now can move

--- src/enemies.js
+++ src/enemies.js
@@ -2,11 +2,12 @@ function Enemy() {
     // main properties
 
     this.speed;
-    this.damageSpeeed;
+    this.damageSpeed;
     this.damage;
     this.hp;
 
     // special powers
+    // TODO: realise special powers
 
     this.canShoot;
     this.shootDistance;
@@ -24,14 +25,44 @@ function Enemy() {
 
     this.element;
 
+    // timing
+
+    var lastShot = 0;
+    var lastMove = 0;
+
+    // path finding
+
+    var lastWay = [];
+
     this.move = function (offset) {
-        //
+        lastMove = lastMove + offset;
+        if (lastMove >= 1000 / this.speed) {
+            lastMove = lastMove - 1000 / this.speed;
+            this.go();
+        }
+    }
+    this.go = function () {
+        lastWay = way([this.posX, this.posY], lastWay, this.currentTarget);
+
+        var pos = lastWay.shift();
+
+        this.posX = pos[0];
+        this.posY = pos[1];
+
+        // animate
     }
     this.attack = function (offset) {
-        //
+        lastShot = lastShot + offset;
+        if (lastShot >= 1000 / this.damageSpeed) {
+            lastShot = lastShot - 1000 / this.damageSpeed;
+            this.fire();
+        }
     }
     this.fire = function () {
-        //
+        if (Math.abs(this.currentTarget.posX - this.posX) === 1 || Math.abs(this.currentTarget.posY - this.posY) === 1) {
+            this.currentTarget.hurt(this.damage);
+            // animation
+        }
     }
     this.target = function () {
         var targets = towersInRange([this.posX, this.posY]);
--- src/path.js
+++ src/path.js
@@ -1,18 +1,17 @@
 findEnemies = {
 
 }
-function way(start, last) {
-    var target;
-    if (target = towersInRange(start)) {
+function way(start, last, target) {
+    if (target) {
         if (last[last.length - 1] === target[target.length - 1].pos) {
             return last;
         } else {
             return finder.findPath(start[0], start[1], target[target.length - 1].pos[0], target[target.length - 1].pos[1]);
         }
     }
-    var first_target = [Math.floor(map[0].length / 2), Math.floor(map.length / 2)];
+    var first_target = [Math.floor(map.width / 2), Math.floor(map.height / 2)];
     if (last[last.length - 1] == first_target) {
         return last;
     }
-    return finder.findPath(start[0], start[1], Math.floor(map[0].length / 2), Math.floor(map.length / 2));
+    return finder.findPath(start[0], start[1], Math.floor(map.width / 2), Math.floor(map.height / 2));
 }
