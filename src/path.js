95e3464ee7465f373eae485eaa7e84fb6cda7db0
Panov Paul <paulll@derpy.ru>
2013-12-20 07:05:11 +0400

    bugfixes

--- src/path.js
+++ src/path.js
@@ -9,12 +9,12 @@ function way(start, last, target) {
         if (last[last.length - 1] === [target.posX, target.posY]) {
             return last;
         } else {
-            return finder.findPath(start[0], start[1], target.posX, target.posY);
+            return finder.findPath(start[0], start[1], target.posX, target.posY, pathGrid);
         }
     }
     var first_target = [Math.floor(map.width / 2), Math.floor(map.height / 2)];
     if (last[last.length - 1] == first_target) {
         return last;
     }
-    return finder.findPath(start[0], start[1], Math.floor(map.width / 2), Math.floor(map.height / 2));
+    return finder.findPath(start[0], start[1], Math.floor(map.width / 2), Math.floor(map.height / 2), pathGrid);
 }
